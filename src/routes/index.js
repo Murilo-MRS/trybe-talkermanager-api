f81bee6f92149dba18fd8c1e95c5af8618e6e8c6
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-14 17:29:58 -0300

    feat: req 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,29 @@ const router = express.Router();
 const { readFile } = require('../utils/fileHandle');
 
 router.get('/', async (_req, res) => {
-  const talker = await readFile();
-
-  if (!talker) {
+  const talkers = await readFile();
+  
+  if (!talkers) {
     return res.status(200).json([]);
   }
 
-  res.status(200).json(talker);
+  res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  
+  const talker = talkers.find((e) => e.id === +id);
+
+  if (!talker) {
+    return res.status(404).json(
+      {
+        message: 'Pessoa palestrante não encontrada',
+      },
+    );
+  }
+  return res.status(200).json(talker);
 });
 
 module.exports = router;
