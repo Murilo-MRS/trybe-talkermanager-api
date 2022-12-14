f7d12bf9c3175a5ac7d3b6e43dc74d36fece8e0b
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-14 16:10:17 -0300

    [MURILO RODRIGUES SANTANA]:rocket: Talk Manager

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
+//vasco
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
